fb7fb4929467151806a52649b3424243cd4c6103
Brian <brianjackson1224@gmail.com>
2024-12-23 21:27:14 -0800

    Table and rows of player containers added

--- Web/src/components/Game/Game.tsx
+++ Web/src/components/Game/Game.tsx
@@ -9,6 +9,15 @@ const Game = () => {
         <div className="game-container">
             <div className="gameWindow-container">
                 <div className="game-table"></div>
+                <div className="game-rowOfPlayers">
+                    Hello
+                </div>
+                <div className="game-rowOfPlayers">
+                    Hello
+                </div>
+                <div className="game-rowOfPlayers">
+                    Hello
+                </div>
             </div>
             <GameControls/>
         </div>

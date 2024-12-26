3851cdbb4d27224801034abb2afaa5547f067447
Brian <brianjackson1224@gmail.com>
2024-12-25 16:43:21 -0800

    Create Deck in constructor

--- Api/Hubs/GameHub.cs
+++ Api/Hubs/GameHub.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.SignalR;
+using Api.Game.Helpers;
+using Api.Game.Models;
 
 namespace Api.Hubs;
 
 public class GameHub : Hub
 {
+    public List<Card> deck;
     public GameHub()
     {
-
+        deck = CardFactory.CreateDeck();
     }
 
     public async Task SendMessage(string user, string message)

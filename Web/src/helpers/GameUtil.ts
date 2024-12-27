e3222bbb605b93f1b211b47c862a02ce5a8bd098
Brian <brianjackson1224@gmail.com>
2024-12-26 22:02:01 -0800

    Fixed abbreviateChips bug

--- Web/src/helpers/GameUtil.ts
+++ Web/src/helpers/GameUtil.ts
@@ -4,10 +4,10 @@ export const abbreviateChips = (chips: number): string => {
 
     if (chipString.length > 3 && chipString.length < 7) {
         let lastIndex = chipString.length - 3;
-        abbreviation = `${chipString.substring(0, lastIndex)}${chipString[lastIndex] !== '0' && `.${chipString[lastIndex]}`}k`;
+        abbreviation = `${chipString.substring(0, lastIndex)}${chipString[lastIndex] !== '0' ? `.${chipString[lastIndex]}` : ""}k`;
     } else if (chipString.length >= 7) {
         let lastIndex = chipString.length - 6;
-        abbreviation = `${chipString.substring(0, lastIndex)}${chipString[lastIndex] !== '0' && `.${chipString[lastIndex]}`}m`;
+        abbreviation = `${chipString.substring(0, lastIndex)}${chipString[lastIndex] !== '0' ? `.${chipString[lastIndex]}` : ""}m`;
     } else {
         abbreviation = chipString;
     }

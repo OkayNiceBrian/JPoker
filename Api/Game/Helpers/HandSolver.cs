574455490f731ebd5a8dcaf02ab7bb99ea18d002
Brian Jackson <brianjackson1224@gmail.com>
2025-01-11 21:09:22 -0800

    We're checking for pairs now!

--- Api/Game/Helpers/HandSolver.cs
+++ Api/Game/Helpers/HandSolver.cs
@@ -24,11 +24,96 @@ public static class HandSolver
             cardPool.Add(player.Card2!);
 
             // Logic to figure out hands right here :)
+            Hand? hand = CheckPairs(cardPool);
 
             cardPool.Remove(player.Card1!);
             cardPool.Remove(player.Card2!);
         }
 
         return winningPlayer;
+    }
+
+    private static Hand CheckPairs(List<Card> cardPool)
+    {
+        int maxOccurences = 0;
+        Rank maxRank = new()
+        {
+            Value = 2,
+            ToChar = "2",
+            ToString = "two"
+        };
+        for (int i = 0; i < cardPool.Count; i++)
+        {
+            int occurences = 0;
+            Rank? rank = null;
+            for (int j = 0; j < cardPool.Count; j++)
+            {
+                if (cardPool[i].Rank == cardPool[j].Rank)
+                {
+                    occurences++;
+                }
+            }
+            if (occurences > maxOccurences)
+            {
+                maxOccurences = occurences;
+                maxRank = cardPool[i].Rank;
+            } else if (occurences == maxOccurences && rank! > maxRank)
+            {
+                maxRank = rank!;
+            }
+        }
+
+        if (maxOccurences == 4)
+        {
+            return new Quads(maxRank);
+        }
+
+        if (maxOccurences == 3 || maxOccurences == 2)
+        {
+            int maxOccurences2 = 0;
+            Rank maxRank2 = new()
+            {
+                Value = 2,
+                ToChar = "2",
+                ToString = "two"
+            };
+            for (int i = 0; i < cardPool.Count; i++)
+            {
+                int occurences = 0;
+                Rank? rank = null;
+                for (int j = 0; j < cardPool.Count; j++)
+                {
+                    if (cardPool[i].Rank == cardPool[j].Rank && cardPool[i].Rank != maxRank)
+                    {
+                        occurences++;
+                    }
+                }
+                if (occurences > maxOccurences2)
+                {
+                    maxOccurences2 = occurences;
+                    maxRank2 = cardPool[i].Rank;
+                }
+                else if (occurences == maxOccurences2 && rank! > maxRank2)
+                {
+                    maxRank2 = rank!;
+                }
+            }
+
+            if (maxOccurences2 >= 2)
+            {
+                if (maxOccurences == 3)
+                {
+                    return new FullHouse(maxRank, maxRank2);
+                }
+                else
+                {
+                    return new TwoPair(maxRank, maxRank2);
+                }
+            }
+
+            return new Pair(maxRank);
+        }
+
+        return new HighCard(maxRank);
     }
 }
--- Api/Game/Models/Rank.cs
+++ Api/Game/Models/Rank.cs
@@ -2,18 +2,29 @@ using System.Numerics;
 
 namespace Api.Game.Models;
 
-public class Rank : IComparable<Rank>
+public class Rank
 {
     public int Value { get; set; }
     public string ToString { get; set; }
     public string ToChar { get; set; }
 
-    public int CompareTo(Rank? that)
+    public static bool operator >(Rank a, Rank b)
     {
-        if (that == null)
-        {
-            return 1;
-        }
-        return this.Value - that.Value;
+        return a.Value > b.Value;
+    }
+
+    public static bool operator <(Rank a, Rank b)
+    {
+        return a.Value < b.Value;
+    }
+
+    public static bool operator ==(Rank a, Rank b)
+    {
+        return a.Value == b.Value;
+    }
+
+    public static bool operator !=(Rank a, Rank b)
+    {
+        return a.Value != b.Value;
     }
 }

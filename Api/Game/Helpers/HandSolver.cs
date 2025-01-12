732bc8925688fdec4643520767055c16023cf0cc
Brian Jackson <brianjackson1224@gmail.com>
2025-01-11 20:41:31 -0800

    Starting HandSolver logic

--- Api/Game/Helpers/HandSolver.cs
+++ Api/Game/Helpers/HandSolver.cs
@@ -4,11 +4,31 @@ namespace Api.Game.Helpers;
 
 public static class HandSolver
 {
-    public static void DetermineWinner(this Lobby lobby)
+    public static Player DetermineWinner(this Lobby lobby)
     {
         var players = lobby.Players;
         var communityCards = lobby.CommunityCards;
+        List<Card> cardPool = communityCards.Select(c => c).ToList();
+
+        Player winningPlayer = players[0];
+        Hand winningHand = new HighCard(new Rank
+        {
+            Value = 2,
+            ToChar = "2",
+            ToString = "two"
+        });
         
-        
+        foreach(var player in players)
+        {
+            cardPool.Add(player.Card1!);
+            cardPool.Add(player.Card2!);
+
+            // Logic to figure out hands right here :)
+
+            cardPool.Remove(player.Card1!);
+            cardPool.Remove(player.Card2!);
+        }
+
+        return winningPlayer;
     }
 }
